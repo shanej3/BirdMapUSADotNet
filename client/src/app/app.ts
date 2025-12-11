c9cd93e24ac44e804e5d3d446fe5a4d73e6b8b7e
Shane <shanejen56@gmail.com>
2025-12-10 16:10:25 -0800

    setup tailwind/daisyUI, basic styling + API call

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,28 +1,31 @@
+import { JsonPipe } from '@angular/common';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, Signal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 // root component
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App implements OnInit {
   private http = inject(HttpClient);
   protected title = signal('BirdMapUSA');
+  protected birds = signal<any>([]);
 
   ngOnInit(): void {
     const params = new HttpParams()
-      .set('lat', 34.0522)
-      .set('lng', -118.2437)
-      .set('radiusKm', 10);
+      .set('lat', '34.0522')
+      .set('lng', '-118.2437')
+      .set('radiusKm', '10');
     this.http.get('http://localhost:5002/api/ebird/NearbyObservations', { params }).subscribe({
-      next: response => console.log('API Response:', response),
+      next: response => this.birds.set(response),
       error: error => console.log(error),
       complete: () => console.log('API call completed')
     })
   }
+  
 }
